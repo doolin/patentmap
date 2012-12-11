65d8468ff2b553beeaca5947af170cbfc60e1e6d
Dave Doolin <david.doolin@gmail.com>
2012-12-10 18:41:12 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Patentmap::Application.config.secret_token = '82e0c888d2f09f930ad4f6570483a556b8fb9659a9b67b4fd207fada31ab94dee49d8c583d93eebdc212f183f2a4b9534870873f0093979c6719e8ccb3637e7f'

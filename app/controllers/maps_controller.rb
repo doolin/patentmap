1e980f5cb58c7a489d1cb1524344d10deb057d41
Adam Barber <adam@adambarber.tv>
2012-12-10 19:42:33 -0800

    Styled home page

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,9 +2,14 @@ require 'httparty'
 
 class MapsController < ApplicationController
   def index
-    lastname = "BAKER"
-    firstname = "ROBERT"
-    key = "1ssyy5yBbx"
+    # lastname = "BAKER"
+    # firstname = "ROBERT"
+    # key = "1ssyy5yBbx"
+    
+    lastname = params[:lastname] if params[:lastname]
+    firstname = params[:firstname] if params[:firstname]
+    key = params[:key]
+  
     @response = HTTParty.get("http://patentsnapper.com/api/invpats?lastname=#{lastname}&firstname=#{firstname}&key=#{key}")
   end
 end

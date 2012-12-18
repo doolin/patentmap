28f208459854357927ae64c2a31ba5ac50f0fa18
Dave Doolin <david.doolin@gmail.com>
2012-12-18 12:28:26 -0800

    added view testing classes and helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,26 @@
 module ApplicationHelper
+
+  def template_id
+    if params[:controller] == "high_voltage/pages"
+      {:id => params[:id] }
+    else
+      {:id => params[:action] }
+    end
+
+  end
+
+  def template_class
+    unless params[:controller].nil?
+      {:class => "#{params[:controller].gsub(/\//,'-')} #{params[:action]}"}
+    else
+      {:class => "FIXME"}
+    end
+  end
+
+  def is_public_home?
+    if params[:controller] == 'pages' && params[:action] == "index" && !current_user
+      true
+    end
+  end
+
 end

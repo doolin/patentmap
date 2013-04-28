6a8cbe3768858d0211b9de8c45679848e18e2b35
Dave Doolin <david.doolin@gmail.com>
2013-04-28 16:46:25 -0700

    cleaning up the maps controller

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -2,16 +2,14 @@ require 'httparty'
 
 class MapsController < ApplicationController
   def index
-    # lastname = "BAKER"
-    # firstname = "ROBERT"
-    # key = "1ssyy5yBbx"
- 
+
     lastname = URI.encode(params[:lastname]) if params[:lastname]
     firstname = URI.encode(params[:firstname]) if params[:firstname]
     key = params[:key]
-  
-    @response = HTTParty.get("http://patentsnapper.com/api/invpats?lastname=#{lastname}&firstname=#{firstname}&key=#{key}")
-    
+
+    query_string = "lastname=#{lastname}&firstname=#{firstname}&key=#{key}"
+    @response = HTTParty.get("http://patentsnapper.com/api/invpats?#{query_string}")
+
     respond_to do |format|
       format.html # render index.html
       format.json { render json: @response.parsed_response }
